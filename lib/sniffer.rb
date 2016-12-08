8585381bbe7e4684c5afc3a2b9745d69b8c6fef7
Matt Wagner <matt.a.wagner4@gmail.com>
2016-12-07 22:08:06 -0500

    switch from Chatter

---
+++ lib/sniffer.rb
@@ -0,0 +1,7 @@
+module Stylesniff
+  class Sniffer < Base
+    def initialize
+      puts 'Initializing Gem'
+    end
+  end
+end
--- lib/stylesniff.rb
+++ lib/stylesniff.rb
@@ -1,9 +1,35 @@
+require "fileutils"
 require "stylesniff/version"
 
 module Stylesniff
-  class Chatter
-    def say_hello
-      puts 'Sniff-Sniff'
+  class Base
+    # def send_to_log(filename)
+    #   return if filename.include?('/gems/')
+    #
+    #   # Debugger stuff
+    #   if filename.include?('.css') || filename.include?('.scss')
+    #     # Build tracer hash
+    #     imps = File.open(filename) do |file|
+    #       file.find_all { |line| line =~ /@import/ }
+    #     end
+    #
+    #     reqs = File.open(filename) do |file|
+    #       file.find_all { |line| line =~ /[*]= require/ }
+    #     end
+    #
+    #     h = { :file => filename, :imports => imps, :requires => reqs }
+    #     if h[:imports].present? || h[:requires].present?
+    #       $require_logger.info("#{h[:file]}\r\n-------------------\r\n\trequires: \r\n\t\t#{h[:requires].join("\t\t") unless !h[:requires].present?}\r\n\timports: \r\n\t\t#{h[:imports].join("\t\t") unless !h[:imports].present?}")
+    #     end
+    #
+    #     # Check for duplications
+    #     if $require_duplications.exclude?(filename)
+    #       $require_duplications << filename
+    #     end
+    #   end
+    # end
+    def ping
+      puts 'pong'
     end
   end
 end
